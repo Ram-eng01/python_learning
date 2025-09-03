063dbfeff85a13544b510fc5ab5454fe680d397b
Ram-eng01 <rammohan63025@gmail.com>
2025-09-03 13:08:14 +0530

    python

---
+++ Day-3/cal-function.py
@@ -0,0 +1,18 @@
+num1 = 44
+num2 = 5
+
+def addition():
+    add = num1 + num2
+    print(add)
+
+def sub():
+    s = num1 - num2
+    print(s)
+
+def mul():
+    m = num1 * num2
+    print(m)
+
+addition()
+sub()
+mul()
